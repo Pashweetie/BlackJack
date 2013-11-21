0ac5eadf0612f275e294ccb8b1b902702286db5f
Treece <Pashweetie@gmail.com>
2013-11-21 09:14:43 -0700

    Well everything is pretty well screwed up

--- BlackJack.java
+++ BlackJack.java
@@ -1,15 +1,60 @@
+import javax.swing.*;
 import java.awt.*;
-import java.applet.*;
-import java.awt.Graphics;
-public class BlackJack extends Applet {
+import java.awt.event.*;
+
+public class NumberPanel extends JPanel implements ActionListener, MouseListener {
+	
+
+	private int value = 0;
+
+	public NumberPanel() {
+		super();
+
+		label = new JLabel(value+"");
+		label.setFont(new Font("sansserif", Font.BOLD, 32));
+		this.add(label);
+
+
+		this.addMouseListener(this);
+	}
+	public void actionPerformed(ActionEvent ae) {
+		if("+1".equals(ae.getActionCommand())){
+			value++;
+			label.setText(value+"");
+			repaint();
+		}else if ("-1".equals(ae.getActionCommand())) {
+			value --;
+			label.setText(value+"");
+			repaint();
+		}
+	}
+public class BlackJack extends Applet implements ActionListener {
+	private JLabel label;
+	private JButton add, stay;
+	private Boolean gameOver = false;
 	Dealer dealer;
-	Human human;
-	Buttons buttons; 		
+	Human human;			
 	public void init() {
 		dealer = new Dealer();
 		human = new Human();
-		buttons = new Buttons();
+		String title = "add";
+		add = new JButton(title);
+		add.setActionCommand(title);
+		add.addActionListener(this);
+		this.add(add);
+
+		title = "stay";
+		stay = new JButton(title);
+		stay.setActionCommand(title);
+		stay.addActionListener(this);
+		this.add(stay);
+
 		System.out.println("BlackJack init is running");
+	}
+	public void actionPerformed(ActionEvent ae) {
+		if("stay".equals(ae.getActionCommand())){
+			gameOver=true;
+		}
 	}
 	public void paint(Graphics g) {			
 		dealer.paint(g);
--- Card.java
+++ Card.java
@@ -49,33 +49,5 @@ public class Card  {
 		g.drawImage(image, x, y, width, height, null);
 		System.out.println("Cards drawing function works");
 	}
-	// public class MoviePoster {
-
-	// 	private String name;
-	// 	private Image image;
-
-	// 	public MoviePoster(String name) {
-	// 		this.name = name;
-	// 		this.image = MoviePoster.loadImage(name);
-	// 	}
-	// 	public void Draw(Graphics g, Rectangle r) {
-	// 		g.drawImage(image, r.x, r.y, r.width, r.height, null);
-	// 	}
-	// 	private static Image loadImage(String name) {
-	// 		String path = null;
-	// 		Image image = null;
-
-	// 		try{
-	// 			path = "images" + File.separator + name + ".jpg";
-	// 			// path = "images/awegaeee.jpg";
-	// 			image = ImageIO.read(new File(path));
-	// 		}catch(IOException e){
-	// 			System.out.println("Could not load image at path:" +path);
-	// 			System.exit(1);
-	// 		}
-
-	// 		return image;
-
-	// 	}
-	// }
+	
 }
